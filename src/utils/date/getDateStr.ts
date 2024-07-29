77b241ea92bce52152319d3fd9db509665bd9bea
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2024-07-29 12:33:22 +0600

    transform: utils from js to ts

--- src/utils/date/getDateStr.ts
+++ src/utils/date/getDateStr.ts
@@ -1,18 +1,18 @@
 import { format, formatDistanceToNow, parseISO } from 'date-fns'
 
-export function getDateStr(isoDateStr) {
+export function getDateStr(isoDateStr: string): string {
   const date = isoDateStr && parseISO(isoDateStr)
   const formattedDate = date && format(date, 'MMMM d, yyyy')
   return formattedDate
 }
 
-export function getRelativeTime(isoDateStr) {
+export function getRelativeTime(isoDateStr: string): string {
   const date = isoDateStr && parseISO(isoDateStr)
   const relativeTime = date && formatDistanceToNow(date, { addSuffix: true })
   return relativeTime
 }
 
-export function getRelativeTimmeFromTimestamp(timestamp) {
+export function getRelativeTimmeFromTimestamp(timestamp: number) {
   const date = new Date(timestamp)
   return formatDistanceToNow(date) + ' ago'
 }
--- src/utils/date/setMaxDate.ts
+++ src/utils/date/setMaxDate.ts
@@ -1,8 +1,8 @@
-export const setMaxDate = (maxYear = 0) => {
+export const setMaxDate = (maxYear: number = 0): string => {
   const today = new Date()
   const year = today.getFullYear()
-  let month = today.getMonth() + 1
-  let day = today.getDate()
+  let month: string | number = today.getMonth() + 1
+  let day: string | number = today.getDate()
   month = month < 10 ? '0' + month : month
   day = day < 10 ? '0' + day : day
   return `${year - maxYear}-${month}-${day}`
