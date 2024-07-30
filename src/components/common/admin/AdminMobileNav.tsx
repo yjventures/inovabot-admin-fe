57dcdd582cfa15cb0e8509372b47f86448fbaa98
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2024-07-30 13:13:40 +0600

    add: border for mobile nav

--- src/components/common/admin/AdminMobileNav.tsx
+++ src/components/common/admin/AdminMobileNav.tsx
@@ -41,7 +41,7 @@ export default function AdminMobileNav({ user, navbarOpen, setnavbarOpen }: Prop
   return (
     <nav
       className={cn(
-        'fixed top-0 right-0 bg-white w-[264px] h-screen flex flex-col items-center justify-between transition-all duration-500 z-50 overflow-y-auto',
+        'fixed top-0 right-0 bg-white w-[264px] h-screen flex flex-col items-center justify-between transition-all duration-500 z-50 overflow-y-auto border-l border-r-gray-primary',
         {
           '-right-[264px]': !navbarOpen
         }
