5fe5efe415c550de417e0986d4ad40031ca61a7b
abraranwar09 <abraranwar09@gmail.com>
2024-09-05 17:01:25 +0600

    strict fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
   images: {
     domains: ['rfqbucket.s3-website-ap-southeast-2.amazonaws.com']
-  }
+  },
+  reactStrictMode: false //fix react strict mode -abrar
 }
 
 export default nextConfig
