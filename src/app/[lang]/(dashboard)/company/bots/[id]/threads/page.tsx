083817118718880a32b17c9b18ee3522b294213d
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2024-10-17 12:55:05 +0600

    add: threads showing for company and reseller

---
+++ src/app/[lang]/(dashboard)/company/bots/[id]/threads/page.tsx
@@ -0,0 +1,9 @@
+import BotThreads from '@/components/common/admin/BotThreads'
+
+export const metadata = {
+  title: 'Bot Threads'
+}
+
+export default function BotThreadPage() {
+  return <BotThreads />
+}
